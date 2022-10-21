2da4428f0be11e44f3e439c22070497bb3ca1145
xwzhang <929554298@qq.com>
2022-10-21 21:39:17 +0800

    hot-fix commit 2

--- src/main/java/com/xwzhang/git/GitTest.java
+++ src/main/java/com/xwzhang/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("git");
         System.out.println("git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix");
     }
 }
